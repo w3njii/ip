559789c70bb59d15389a2533adfe63a9e1d54454
w3njii <wenjie.chong2018@gmail.com>
2025-08-22 15:21:15 +0800

    Add Echo
    After the greeting, Steven now echos commands entered by the user, and exits when the user types the command "bye".

--- src/main/java/Steven.java
+++ src/main/java/Steven.java
@@ -1,13 +1,22 @@
+import java.util.Scanner;
+
 public class Steven {
     public static void main(String[] args) {
         final String HORIZONTAL_LINE = "____________________________________________________________";
-        final String GREETING = "Hello! I'm Steven\nWhat can I do for you?";
-        final String GOODBYE = "Bye. Hope to see you again soon!";
+        final String GREETING = "Hello There! I'm Steven\nHow may I help you?";
+        final String GOODBYE = "Bye.";
 
-        System.out.println(HORIZONTAL_LINE);
         System.out.println(GREETING);
         System.out.println(HORIZONTAL_LINE);
-        System.out.println(GOODBYE);
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        while (!input.equals("bye")) {
+            System.out.println("\t" + input);
+            input = scanner.nextLine();
+        }
+
         System.out.println(HORIZONTAL_LINE);
+        System.out.println(GOODBYE);
     }
 }
