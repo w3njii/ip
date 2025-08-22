1b5ea5f67d9faaa3bd9e6a607a4140d49b204edf
w3njii <wenjie.chong2018@gmail.com>
2025-08-22 17:04:48 +0800

    Add list
    Add the ability to store whatever text entered by the user and display them back to the user when requested.

--- src/main/java/Steven.java
+++ src/main/java/Steven.java
@@ -1,21 +1,34 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Steven {
+
     public static void main(String[] args) {
         final String HORIZONTAL_LINE = "____________________________________________________________";
         final String GREETING = "Hello There! I'm Steven\nHow may I help you?";
         final String GOODBYE = "Bye.";
 
+        ArrayList<String> toDoList = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println(GREETING);
         System.out.println(HORIZONTAL_LINE);
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
 
-        while (!input.equals("bye")) {
-            System.out.println("\t" + input);
-            input = scanner.nextLine();
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                break;
+            } else if (input.equals("list")) {
+                for (int i = 0; i < toDoList.size(); i++) {
+                    System.out.println("\t" + i + 1 + ". " + toDoList.get(i));
+                }
+                continue;
+            }
+            toDoList.add(input);
+            System.out.println("\t added: " + input);
         }
 
+        scanner.close();
         System.out.println(HORIZONTAL_LINE);
         System.out.println(GOODBYE);
     }
