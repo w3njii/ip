68ac32bc94b9ee22d25599a3794f51aaecef65e9
w3njii <wenjie.chong2018@gmail.com>
2025-08-29 22:08:16 +0800

    Add Javadoc for all Exceptions

--- src/main/java/steven/MissingDeadlineException.java
+++ src/main/java/steven/MissingDeadlineException.java
@@ -1,5 +1,8 @@
 package steven;
 
+/**
+ * Thrown when a user attempts to add a Deadline task without specifying a deadline.
+ */
 public class MissingDeadlineException extends StevenException {
     public MissingDeadlineException() {
         super("\tWhr is your deadline???");
--- src/main/java/steven/MissingStartAndEndTimeException.java
+++ src/main/java/steven/MissingStartAndEndTimeException.java
@@ -1,5 +1,8 @@
 package steven;
 
+/**
+ * Thrown when a user attempts to add an Event without specifying the start or end time.
+ */
 public class MissingStartAndEndTimeException extends StevenException {
     public MissingStartAndEndTimeException() {
         super("\tYour event from when to when????");
