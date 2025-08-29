47151f84d214b1587c386f1d038f577748e23ef0
w3njii <wenjie.chong2018@gmail.com>
2025-08-29 20:41:37 +0800

    Move convert method from Command to Parser

--- src/main/java/steven/Parser.java
+++ src/main/java/steven/Parser.java
@@ -3,6 +3,16 @@ package steven;
 public class Parser {
 
     public Command parse(String input) {
-        return Command.convert(input.split(" ")[0]);
+        return switch (input.toLowerCase()) {
+            case "todo" -> Command.TODO;
+            case "deadline" -> Command.DEADLINE;
+            case "event" -> Command.EVENT;
+            case "mark" -> Command.MARK;
+            case "unmark" -> Command.UNMARK;
+            case "delete" -> Command.DELETE;
+            case "list" -> Command.LIST;
+            case "bye" -> Command.BYE;
+            default -> Command.UNKNOWN;
+        };
     }
 }
